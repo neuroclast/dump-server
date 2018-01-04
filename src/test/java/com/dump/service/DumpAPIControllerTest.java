ff5814dea8c9cce77f0cd86f99630d089d9c09fc
neuroclast@gmail.com <neuroclast@gmail.com>
2018-01-04 16:16:05 -0600

    Added tests for DumpAPIController

--- src/test/java/com/dump/service/DumpAPIControllerTest.java
+++ src/test/java/com/dump/service/DumpAPIControllerTest.java
@@ -6,12 +6,14 @@ import com.dump.service.objects.User;
 import com.dump.service.repositories.DumpRepository;
 import com.dump.service.repositories.UserRepository;
 import com.dump.service.utils.Auth;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +29,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -74,11 +77,11 @@ public class DumpAPIControllerTest {
 
         // configure mocks
         when(dumpRepository.findByPublicId("aaaa"))
-                .thenReturn(null)
-                .thenReturn(mockDump);
+                .thenReturn(null)       // invalid Dump
+                .thenReturn(mockDump);  // valid Dump
 
         when(userRepository.findByUsernameIgnoreCase("josh"))
-                .thenReturn(mockUser);
+                .thenReturn(mockUser);  // valid User
 
         // verify 404 on invalid Dump
         this.mockMvc
@@ -114,8 +117,6 @@ public class DumpAPIControllerTest {
      */
     @Test
     public void testDelete() throws Exception {
-
-        HttpHeaders authHeaders = new HttpHeaders();
         Dump mockDump = new Dump();
         mockDump.setUsername("Josh");
         User wrongMockUser = new User();
@@ -123,16 +124,16 @@ public class DumpAPIControllerTest {
         User mockUser = new User();
         mockUser.setUsername("josh");
 
-        when(authUtil.verifyAuthorization(authHeaders))
-                .thenThrow(new Exception("expired"))
-                .thenReturn(null)
-                .thenReturn(wrongMockUser)
-                .thenReturn(wrongMockUser)
-                .thenReturn(mockUser);
+        when(authUtil.verifyAuthorization(any()))
+                .thenThrow(new Exception("expired"))    // expired JWT
+                .thenReturn(null)                       // invalid user
+                .thenReturn(wrongMockUser)              // unauthorized user
+                .thenReturn(wrongMockUser)              // unauthorized user
+                .thenReturn(mockUser);                  // authorized user
 
         when(dumpRepository.findByPublicId("aaaa"))
-                .thenReturn(null)
-                .thenReturn(mockDump);
+                .thenReturn(null)                       // invalid Dump
+                .thenReturn(mockDump);                  // valid Dump
 
         // test expired JWT
         this.mockMvc
@@ -167,5 +168,173 @@ public class DumpAPIControllerTest {
         verify(authUtil, times(5)).verifyAuthorization(any());
         verify(dumpRepository, times(1)).delete(any(Dump.class));
 
+    }
+
+
+    /**
+     * Tests /update handler
+     * @throws Exception
+     */
+    @Test
+    public void testUpdate() throws Exception {
+        Dump mockDump = new Dump();
+        mockDump.setUsername("Josh");
+        mockDump.setTitle("2018-01-04 15:04:11.455 DEBUG 21248 --- [       Thread-2] o.s.b.f.s.DefaultListableBeanFactory     : Retrieved dependent beans for bean 'org.springframework.boot.autoconfigure.internalCachingMetadataReaderFactory': [org.springframework.context.annotation.internalConfigurationAnnotationProcessor]");
+        mockDump.setPublicId("aaaa");
+        User mockUser = new User();
+        mockUser.setUsername("josh");
+
+        when(authUtil.verifyAuthorization(any()))
+                .thenThrow(new Exception("expired"))    // expired JWT
+                .thenReturn(null)                       // invalid user
+                .thenReturn(mockUser);                  // value user
+
+        // test expired JWT
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/update")
+                            .content(asJsonString(mockDump))
+                            .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isIAmATeapot());
+
+        // test invalid user
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/update")
+                            .content(asJsonString(mockDump))
+                            .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+        // test success with culled title
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/update")
+                                .content(asJsonString(mockDump))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("aaaa"));
+
+        assertThat(mockDump.getTitle().length() == 250);
+        verify(dumpRepository, times(1)).save(any(Dump.class));
+    }
+
+
+    /**
+     * Tests /add handler
+     * @throws Exception
+     */
+    @Test
+    public void testAdd() throws Exception {
+        Dump mockDump = new Dump();
+        mockDump.setUsername("Josh");
+        mockDump.setTitle("2018-01-04 15:04:11.455 DEBUG 21248 --- [       Thread-2] o.s.b.f.s.DefaultListableBeanFactory     : Retrieved dependent beans for bean 'org.springframework.boot.autoconfigure.internalCachingMetadataReaderFactory': [org.springframework.context.annotation.internalConfigurationAnnotationProcessor]");
+        mockDump.setPublicId("aaaa");
+        User mockUser = new User();
+        mockUser.setUsername("josh");
+
+        when(authUtil.verifyAuthorization(any()))
+                .thenThrow(new Exception("expired"))    // expired JWT
+                .thenReturn(null)                       // invalid user
+                .thenReturn(mockUser);                  // success
+
+        when(dumpRepository.findByPublicId(any()))
+                .thenReturn(null);                      // unique public ID
+
+        // test expired JWT
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/add")
+                                .content(asJsonString(mockDump))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isIAmATeapot());
+
+        // test invalid user
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/add")
+                                .content(asJsonString(mockDump))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+        // test success with culled title
+        this.mockMvc
+                .perform(
+                        post("/api/dumps/add")
+                                .content(asJsonString(mockDump))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        assertThat(mockDump.getPublicId().length() == 5);
+        assertThat(mockDump.getTitle().length() <= 250);
+        verify(dumpRepository, times(1)).save(any(Dump.class));
+    }
+
+
+    /**
+     * Tests /user handler
+     * @throws Exception
+     */
+    @Test
+    public void testUser() throws Exception {
+        User mockUser = new User();
+        mockUser.setUsername("josh");
+
+        when(authUtil.verifyAuthorization(any()))
+                .thenThrow(new Exception("expired"))    // expired JWT
+                .thenReturn(null)                       // unauthorized user
+                .thenReturn(mockUser);                  // authorized user
+
+        when(dumpRepository.findFirst100ByUsernameIgnoreCaseOrderByIdDesc("josh"))
+                .thenReturn(new Dump[5])                // auth user
+                .thenReturn(new Dump[5]);               // anon user
+
+        // test expired JWT
+        this.mockMvc
+                .perform(get("/api/dumps/user?username=josh&viewAll=true"))
+                .andDo(print())
+                .andExpect(status().isIAmATeapot());
+
+        // test unauthorized user
+        this.mockMvc
+                .perform(get("/api/dumps/user?username=josh&viewAll=true"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+        // test authorized user
+        this.mockMvc
+                .perform(get("/api/dumps/user?username=josh&viewAll=true"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(dumpRepository, times(1)).findFirst100ByUsernameIgnoreCaseOrderByIdDesc(any());
+
+        // test authorized user
+        this.mockMvc
+                .perform(get("/api/dumps/user?username=josh&viewAll=false"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(dumpRepository, times(1)).findFirst100ByUsernameIgnoreCaseAndExposureOrderByIdDesc(any(), any());
+    }
+
+
+    /**
+     * Returns JSONified version of passed Object
+     * @param obj Object to serialize to JSON
+     * @return String representation of serialized object
+     */
+    private String asJsonString(Object obj) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
