3e49cb6b99aeb7b1183bd30fdecbc16b8e3981ab
neuroclast@gmail.com <neuroclast@gmail.com>
2018-01-04 14:13:19 -0600

    Added tests for DumpAPIController /delete method;

--- src/test/java/com/dump/service/DumpAPIControllerTest.java
+++ src/test/java/com/dump/service/DumpAPIControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,8 +20,12 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -45,8 +50,13 @@ public class DumpAPIControllerTest {
     @MockBean
     private Auth authUtil;
 
+
+    /**
+     * Tests /view handler
+     * @throws Exception
+     */
     @Test
-    public void checkView() throws Exception {
+    public void testView() throws Exception {
 
         String string = "January 1, 2050";
         DateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
@@ -62,22 +72,21 @@ public class DumpAPIControllerTest {
         mockUser.setUsername("josh");
         mockUser.setViews(2);
 
-        // verify 404 on invalid Dump
+        // configure mocks
         when(dumpRepository.findByPublicId("aaaa"))
-                .thenReturn(null);
+                .thenReturn(null)
+                .thenReturn(mockDump);
 
+        when(userRepository.findByUsernameIgnoreCase("josh"))
+                .thenReturn(mockUser);
+
+        // verify 404 on invalid Dump
         this.mockMvc
                 .perform(get("/api/dumps/view/aaaa"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
 
         // verify valid Dump request
-        when(dumpRepository.findByPublicId("aaaa"))
-                .thenReturn(mockDump);
-
-        when(userRepository.findByUsernameIgnoreCase("josh"))
-                .thenReturn(mockUser);
-
         this.mockMvc
                 .perform(get("/api/dumps/view/aaaa"))
                 .andDo(print())
@@ -96,5 +105,67 @@ public class DumpAPIControllerTest {
                 .andExpect(header().string("Content-Type", "text/plain"))
                 .andExpect(header().string("Content-disposition", "attachment; filename=\"aaaa.txt\""))
                 .andExpect(content().string("contents"));
+    }
+
+
+    /**
+     * Tests /delete handler
+     * @throws Exception
+     */
+    @Test
+    public void testDelete() throws Exception {
+
+        HttpHeaders authHeaders = new HttpHeaders();
+        Dump mockDump = new Dump();
+        mockDump.setUsername("Josh");
+        User wrongMockUser = new User();
+        wrongMockUser.setUsername("not-josh");
+        User mockUser = new User();
+        mockUser.setUsername("josh");
+
+        when(authUtil.verifyAuthorization(authHeaders))
+                .thenThrow(new Exception("expired"))
+                .thenReturn(null)
+                .thenReturn(wrongMockUser)
+                .thenReturn(wrongMockUser)
+                .thenReturn(mockUser);
+
+        when(dumpRepository.findByPublicId("aaaa"))
+                .thenReturn(null)
+                .thenReturn(mockDump);
+
+        // test expired JWT
+        this.mockMvc
+                .perform(delete("/api/dumps/delete?publicId=aaaa"))
+                .andDo(print())
+                .andExpect(status().isIAmATeapot());
+
+        // test invalid user
+        this.mockMvc
+                .perform(delete("/api/dumps/delete?publicId=aaaa"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+        // test invalid dump
+        this.mockMvc
+                .perform(delete("/api/dumps/delete?publicId=aaaa"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        // test unauthorized user
+        this.mockMvc
+                .perform(delete("/api/dumps/delete?publicId=aaaa"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+        // test success
+        this.mockMvc
+                .perform(delete("/api/dumps/delete?publicId=aaaa"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(authUtil, times(5)).verifyAuthorization(any());
+        verify(dumpRepository, times(1)).delete(any(Dump.class));
+
     }
 }
